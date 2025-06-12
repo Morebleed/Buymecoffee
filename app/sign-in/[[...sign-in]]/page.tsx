23c9d77ff718e82cb6afafae9c026cf51c792083
Morebleed <deegiidk@gmail.com>
2025-06-12 12:56:55 +0800

    idea

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
 
-export default function Page() {
-  return <SignIn />
+export default function SignInPage() {
+  return <div>
+    <div className='w-710px h-944px backgroundcolor #FBBF24'></div>
+    <div className='w-710px h-944px backgroundcolor #FFFFFF'></div>
+  </div>
+
 }
